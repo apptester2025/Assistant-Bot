3e8519a482071496343093a74a289f142acc57c9
Ryan Price <ppdrmonitor@gmail.com>
2025-01-14 19:00:19 -0600

    updated bot code

--- blueprints/api.py
+++ blueprints/api.py
@@ -1,16 +1,8 @@
-import os
-import requests
 from flask import Blueprint, request, jsonify
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
 # Define the Blueprint
 api = Blueprint('api', __name__)
-
-# Load environment variable for GroupMe bot ID
-GROUPME_BOT_ID = os.getenv("GROUPME_BOT_ID")
 
 @api.route('/discord-invite', methods=['GET'])
 def discord_invite():
