f63ccf892daf6ecba4ccb42ecab81fb523199c28
Ryan Price <ppdrmonitor@gmail.com>
2025-01-26 09:50:05 -0600

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
                 Contact Team
               </a>
               <a
-                href="https://github.com/ppdrmonitor/assistant-bot"
+                href="https://github.com/apptester2025/assistant-bot"
                 className="px-8 py-3 bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-white rounded-lg hover:bg-gray-300 dark:hover:bg-gray-700 transition-colors flex items-center"
               >
                 <Github className="w-5 h-5 mr-2" />
